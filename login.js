e748b19043d6c4cc004234525f86a20f2779ef07
Paul Barrera <paul@paulbarrera.com>
2020-07-16 15:07:35 -0400

    updated readme

--- login.js
+++ login.js
@@ -16,17 +16,15 @@ document.getElementById('currentDate').value = formatDate(date);
 
 //TODO: drivers name input//
 
-let driver = new Driver();
+//TODO: Input outset and destination odometer readings//
 
+//TODO: Calculate total amount of business usage miles (BUM) driven//
 
+//TODO: Calculate total amount of yearly miles//
 
+//TODO: Divide BUM by yearly total miles
 
-
-//TODO: Input outset and destination odometer readings
-
-
-//TODO: Calculate total amount of business usage miles (BUM) driven 
-
+//TODO: Countdown Timer til tax day//
 
 
 
